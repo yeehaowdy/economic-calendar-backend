75c62a88aacde3b680a6f93d0c31979a3a905503
botondterenyi <botiterenyi@gmail.com>
2026-03-19 07:48:21 +0100

    fix origin

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
+    'https://insight-web.netlify.app'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
